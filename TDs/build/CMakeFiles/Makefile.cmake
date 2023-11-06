7e538e18eb30e4ad4a50db9ee1c0511506f35a68
yizhi <yizhi.yang@ens.uvsq.fr>
2023-11-06 10:57:15 +0100

    trial

--- TDs/build/CMakeFiles/Makefile.cmake
+++ TDs/build/CMakeFiles/Makefile.cmake
@@ -15,7 +15,7 @@ set(CMAKE_MAKEFILE_DEPENDS
   "CMakeFiles/3.22.1/CMakeCCompiler.cmake"
   "CMakeFiles/3.22.1/CMakeCXXCompiler.cmake"
   "CMakeFiles/3.22.1/CMakeSystem.cmake"
-  "../mylib_dir/CMakeLists.txt"
+  "../mylib_dir_TD1/CMakeLists.txt"
   "/usr/share/cmake-3.22/Modules/CMakeCInformation.cmake"
   "/usr/share/cmake-3.22/Modules/CMakeCXXInformation.cmake"
   "/usr/share/cmake-3.22/Modules/CMakeCommonLanguageInclude.cmake"
@@ -54,11 +54,12 @@ set(CMAKE_MAKEFILE_OUTPUTS
 # Byproducts of CMake generate step:
 set(CMAKE_MAKEFILE_PRODUCTS
   "CMakeFiles/CMakeDirectoryInformation.cmake"
-  "mylib_dir/CMakeFiles/CMakeDirectoryInformation.cmake"
+  "mylib_dir_TD1/CMakeFiles/CMakeDirectoryInformation.cmake"
   )
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/Main.exe.dir/DependInfo.cmake"
   "CMakeFiles/Test.exe.dir/DependInfo.cmake"
+  "mylib_dir_TD1/CMakeFiles/mylib.dir/DependInfo.cmake"
   )
