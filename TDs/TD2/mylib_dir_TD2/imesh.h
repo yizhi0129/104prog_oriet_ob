8ae0caa47478ca8e2b0fae6b023b7e1cf7819d89
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2023-11-23 13:56:59 +0100

    update

--- TDs/TD2/mylib_dir_TD2/imesh.h
+++ TDs/TD2/mylib_dir_TD2/imesh.h
@@ -3,6 +3,14 @@
 
 class IMesh 
 {
+private:
+    double t_ini;
+    double t_final;
+    double dt;
+    double x_min;
+    double x_max;
+    double dx;
+
 public:
     virtual double initial_time() const = 0;
     virtual double final_time() const = 0;
@@ -10,6 +18,10 @@ public:
     virtual double initial_position() const = 0;
     virtual double final_position() const = 0;
     virtual double position_step() const = 0;
+
+    virtual int x_size() const = 0;
+    
+    virtual ~IMesh() = default;
 };
 
 #endif
--- TDs/TD2/mylib_dir_TD2/problem.cpp
+++ TDs/TD2/mylib_dir_TD2/problem.cpp
@@ -7,11 +7,11 @@ void Problem::solve()
 {
     std::cout << "--- Solve problem ---" << std::endl;
     
-    for (double time = mesh_->getInitialTime(); time <= mesh_->getFinalTime(); time += mesh_->getTimeStep()) 
+    for (double time = mesh_->initial_time(); time <= mesh_->final_time(); time += mesh_->time_step()) 
     {
         std::cout << "--- compute equation at time: " << time << " ---" << std::endl;
         
-        for (double position = mesh_->getInitialPosition(); position <= mesh_->getFinalPosition(); position += mesh_->getSpaceStep()) 
+        for (double position = mesh_->initial_position(); position <= mesh_->final_position(); position += mesh_->position_step()) 
         {
             equation_.compute(time, position);
         }
