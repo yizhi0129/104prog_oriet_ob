47dc3a64294fa7fff86934365b14c31452a01e6a
YANG Yizhi <yizhi.yang@ens.uvsq.fr>
2023-11-30 09:42:30 +0100

    new branch

--- TDs/TD2/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yangyizhi/chps2023/TDs/TD2
-# Build directory: /Users/yangyizhi/chps2023/TDs/TD2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Test.cpp "Main.exe")
-set_tests_properties(Test.cpp PROPERTIES  _BACKTRACE_TRIPLES "/Users/yangyizhi/chps2023/TDs/TD2/CMakeLists.txt;26;add_test;/Users/yangyizhi/chps2023/TDs/TD2/CMakeLists.txt;0;")
-subdirs("mylib_dir_TD2")
